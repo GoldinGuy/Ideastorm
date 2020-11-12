2460dd38304bb51bc61194fd33a85f82243ab160
Seth <sethgoldin27@gmail.com>
2020-11-11 22:54:28 -0500

    update card view page

--- client/src/pages/IdeasAccordian.jsx
+++ client/src/pages/IdeasAccordian.jsx
@@ -7,7 +7,6 @@ class IdeasAccordian extends Component {
 		super(props);
 		this.state = {
 			ideas: [],
-			columns: [],
 			isLoading: false
 		};
 	}
@@ -24,36 +23,73 @@ class IdeasAccordian extends Component {
 	};
 
 	generateIdeaCards(ideas) {
-		for (var i = 0; i < ideas.length - 1; i += 3) {
-			var idea_cards;
-			for (var j = i; j < 3; j++) {
-				if (j < ideas.length - 1) {
+		var cards;
+		for (var i = 0; i < ideas.length; i += 3) {
+			let idea_cards;
+			for (var j = i; j < i + 3; j++) {
+				if (j < ideas.length) {
 					idea_cards = [idea_cards, this.generateIdeaCard(ideas[j])];
 				}
 			}
-			return (
-				<div className="flex flex-col w-full mb-8 sm:flex-row">
+			cards = [
+				cards,
+				<div className="flex flex-col w-full xl:mb-8 lg:mb-8 md:mb-8 sm:mb-0 sm:flex-row">
 					{idea_cards}
 				</div>
-			);
+			];
 		}
+
+		return cards;
 	}
 
 	generateIdeaCard(idea) {
+		let rColor;
+		switch (Math.floor(Math.random() * 5) + 1) {
+			case 1:
+				rColor = "indigo-500";
+				break;
+			case 2:
+				rColor = "purple-500";
+				break;
+			case 3:
+				rColor = "blue-400";
+				break;
+			case 4:
+				rColor = "teal-500";
+				break;
+			case 5:
+				rColor = "indigo-500";
+				break;
+			default:
+				rColor = "yellow-400";
+		}
 		return (
 			<div className="w-full mb-10 sm:mb-0 sm:w-1/2">
 				<div className="relative ml-0 mr-0 sm:mr-10">
-					<span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg" />
-					<div className="relative p-5 bg-white border-2 border-indigo-500 rounded-lg">
+					<span
+						className={`absolute top-0 left-0 w-full h-full mt-1 ml-1 rounded-lg bg-${rColor}`}
+					/>
+					<div
+						className={`relative p-5 bg-white border-2 rounded-lg border-${rColor}`}
+					>
 						<div className="flex items-center -mt-1">
-							<h3 className="my-2  text-lg font-bold text-gray-800">
+							<h3 className="my-2 text-lg font-bold text-gray-800">
 								{idea.name}
 							</h3>
 						</div>
-						<p className="mt-3 mb-1 text-xs font-medium text-indigo-500 uppercase">
+						<p
+							className={`mt-1 mb-1 text-xs font-medium uppercase text-${rColor}`}
+						>
 							SHARED 5 Days ago
 						</p>
 						<p className="mb-2 text-gray-600">{idea.description}</p>
+						<h5 class="flex-wrap flex">
+							{idea.tags.map((tag, index) => (
+								<span class="px-1 mb-1 mr-1 text-gray-900 bg-gray-300 text-sm border border-gray-500 rounded-lg">
+									{"#" + tag}
+								</span>
+							))}
+						</h5>
 					</div>
 				</div>
 			</div>
@@ -63,44 +99,24 @@ class IdeasAccordian extends Component {
 	render() {
 		const { ideas, isLoading } = this.state;
 
-		// const columns = [
-		// 	{
-		// 		Header: "ID",
-		// 		accessor: "_id",
-		// 		filterable: true
-		// 	},
-		// 	{
-		// 		Header: "Name",
-		// 		accessor: "name",
-		// 		filterable: true
-		// 	},
-		// 	{
-		// 		Header: "Description",
-		// 		accessor: "description",
-		// 		filterable: true
-		// 	},
-		// 	{
-		// 		Header: "Tags",
-		// 		accessor: "tags",
-		// 		filterable: true,
-		// 		Cell: props => <span>{props.value.join(" / ")}</span>
-		// 	}
-		// ];
-
-		let showTable = true;
+		let showIdeas = true;
 		if (!ideas.length) {
-			showTable = false;
+			showIdeas = false;
 		}
 
 		return (
-			<div className="container relative flex flex-col justify-between h-full max-w-6xl px-8 mx-auto xl:px-0">
-				<h2 className="relative flex items-center self-start inline-block w-auto mb-20 text-4xl font-black">
-					<span className="absolute inline-block w-full h-4 mt-3 -ml-2 bg-yellow-400" />
-					<span className="relative">Trending</span>
-				</h2>
-				<div className="flex w-full h-full">
-					<div className="w-full">{this.generateIdeaCards(ideas)}</div>
-				</div>
+			<div>
+				{showIdeas && (
+					<div className="container relative flex flex-col justify-between h-full max-w-6xl px-8 mx-auto xl:px-0">
+						<h2 className="relative flex items-center self-start inline-block w-auto mb-10 mt-5 text-4xl font-black">
+							<span className="absolute inline-block w-full h-4 mt-3 -ml-2 bg-yellow-400" />
+							<span className="relative">Trending</span>
+						</h2>
+						<div className="flex w-full h-full">
+							<div className="w-full">{this.generateIdeaCards(ideas)}</div>
+						</div>
+					</div>
+				)}
 			</div>
 
 			/*	 <Wrapper>
