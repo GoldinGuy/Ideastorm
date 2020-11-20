694adb2a8b10d1de201af2f6e34a4cb45ec7310c
Seth <sethgoldin27@gmail.com>
2020-11-20 10:02:45 -0500

    titlecase

--- client/src/components/idea_card_components.jsx
+++ client/src/components/idea_card_components.jsx
@@ -56,10 +56,10 @@ const IdeaCard = ({ idea }) => {
 					className={`relative p-5 bg-white border-2 rounded-lg border-${rColor}`}
 				>
 					<div className="flex items-center -mt-1 justify-between">
-						<h3 className="my-2 text-lg font-bold text-gray-800 inline">
+						<h3 className="my-2 text-lg font-bold text-gray-800 inline ">
 							{titleCase(idea.title)}
 						</h3>
-						<div>
+						<div className="inline-block">
 							<span
 								className={`mt-1 mb-1 text-xs font-medium inline uppercase text-${rColor}`}
 							>
@@ -69,7 +69,7 @@ const IdeaCard = ({ idea }) => {
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 320 512"
-								className="w-5 h-5  text-gray-600 fill-current inline"
+								className="w-5 h-5 text-gray-600 fill-current inline"
 							>
 								<path d="M295.973 160H180.572L215.19 30.184C219.25 14.956 207.756 0 192 0H56C43.971 0 33.8 8.905 32.211 20.828l-31.996 240C-1.704 275.217 9.504 288 24.004 288h118.701L96.646 482.466C93.05 497.649 104.659 512 119.992 512c8.35 0 16.376-4.374 20.778-11.978l175.973-303.997c9.244-15.967-2.288-36.025-20.77-36.025z" />
 							</svg>
@@ -150,9 +150,9 @@ const Cards = ({ ideas }) => {
 const IdeasStream = ({ ideas, pageTitle, topTags, history }) => {
 	return (
 		<div className="container relative flex flex-col justify-between h-full max-w-6xl px-8 mx-auto xl:px-0">
-			<div className="relative flex items-center self-start inline-block w-auto mb-1 mt-2   font-black">
+			<div className="relative flex items-center self-start w-auto mb-1 mt-2   font-black">
 				<h2
-					className="relative flex items-center self-start inline-block w-auto  text-4xl font-black mr-4"
+					className="relative flex items-center self-start w-auto  text-4xl font-black mr-4"
 					key="header"
 				>
 					<span className="absolute inline-block w-full h-4 mt-3 -ml-2 bg-yellow-400" />
