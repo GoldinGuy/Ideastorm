d5562693f15fa69578bb6e9f05969355ab6728b7
Seth <sethgoldin27@gmail.com>
2020-11-26 21:58:44 -0500

    table, manifest updates

--- client/src/pages/trending_page.jsx
+++ client/src/pages/trending_page.jsx
@@ -79,6 +79,13 @@ class TrendingPage extends Component {
 							loading={true}
 						/>
 					</div>
+					// 	<div class="flex  justify-center min-h-screen p-5 min-w-screen">
+					// 	<div class="flex space-x-2 animate-pulse">
+					// 		<div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+					// 		<div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+					// 		<div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+					// 	</div>
+					// </div>
 				}
 				className="h-full overflow-y-hidden"
 				endMessage={
