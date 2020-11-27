7d4ccd1456ef9601f8fda3eb193d4c5d10e93910
Seth <sethgoldin27@gmail.com>
2020-11-27 14:57:25 -0500

    more legible button

--- client/src/components/idea_card_components.jsx
+++ client/src/components/idea_card_components.jsx
@@ -91,7 +91,9 @@ const IdeaCard = ({ idea }) => {
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									viewBox="0 0 320 512"
-									className="w-5 h-5 text-gray-600 fill-current inline"
+									className={`w-5 h-5 fill-current inline text-${
+										s_counted ? "yellow-500" : "indigo-600"
+									}`}
 								>
 									<path d="M295.973 160H180.572L215.19 30.184C219.25 14.956 207.756 0 192 0H56C43.971 0 33.8 8.905 32.211 20.828l-31.996 240C-1.704 275.217 9.504 288 24.004 288h118.701L96.646 482.466C93.05 497.649 104.659 512 119.992 512c8.35 0 16.376-4.374 20.778-11.978l175.973-303.997c9.244-15.967-2.288-36.025-20.77-36.025z" />
 								</svg>
