3fd2c600f4f2873455910e11c4b1cff7d4437cf2
Seth <sethgoldin27@gmail.com>
2020-11-27 19:32:26 -0500

    scroll to top on page load

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { CookiesProvider,  } from 'react-cookie';
-import { NavBar, Footer } from '../components'
+import { NavBar, Footer, ScrollToTop } from '../components'
 import { IdeasTable, TrendingPage, NewIdeaPage, LatestPage, HomePage, SearchPage } from '../pages'
 
 
@@ -9,22 +9,23 @@ function App() {
     return (
         <CookiesProvider>
             <BrowserRouter basename="/">
-                <NavBar />
-                <Switch>
-                    {/* <Redirect path="/" to="/"/> */}
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/trending" exact component={TrendingPage} />
-                    <Route path="/latest" exact component={LatestPage} />
-                    <Route path="/search" exact component={SearchPage} />
-                    <Route path="/table" exact component={IdeasTable} />
-                    <Route path="/create" exact component={NewIdeaPage} />
-                    {/* <Route
-                        path="/update/:id"
-                        exact
-                        component={IdeasUpdate}
-                    /> */}
-                </Switch>
-                <Footer />
+                <ScrollToTop>
+                    <NavBar />
+                    <Switch>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/trending" exact component={TrendingPage} />
+                        <Route path="/latest" exact component={LatestPage} />
+                        <Route path="/search" exact component={SearchPage} />
+                        <Route path="/table" exact component={IdeasTable} />
+                        <Route path="/create" exact component={NewIdeaPage} />
+                        {/* <Route
+                            path="/update/:id"
+                            exact
+                            component={IdeasUpdate}
+                        /> */}
+                    </Switch>
+                    <Footer />
+                </ScrollToTop>
             </BrowserRouter>
         </CookiesProvider>
     )
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@
 import NavBar from './nav_bar_component'
 import Footer from './footer_component'
 import IdeasStream from './idea_card_components'
+import ScrollToTop from './scroll_to_top'
 
-export { NavBar, Footer, IdeasStream }
+export { NavBar, Footer, IdeasStream, ScrollToTop }
