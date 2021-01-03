7f844580c441b65e6499239ef173bdc92456114d
Seth <sethgoldin27@gmail.com>
2021-01-03 16:04:15 -0500

    switch to double click to see details

--- client/src/components/idea_card_components.jsx
+++ client/src/components/idea_card_components.jsx
@@ -52,6 +52,8 @@ const IdeaCard = ({ idea, history, pageTitle }) => {
 		// console.log(cookies.s_counted);
 	};
 
+	const shareIdea = () => {};
+
 	if (!idea) {
 		return null;
 	}
@@ -84,7 +86,7 @@ const IdeaCard = ({ idea, history, pageTitle }) => {
 					pageTitle === "Latest" || pageTitle === "Trending" ? "md:w-1/3" : ""
 				}`}
 				key={idea.title}
-				onClick={() => {
+				onDoubleClick={() => {
 					setModal(!modalOpen);
 					history.push({
 						pathname: `/${pageTitle.toLowerCase()}`,
@@ -104,6 +106,19 @@ const IdeaCard = ({ idea, history, pageTitle }) => {
 								{idea.title}
 							</h3>
 							<div className="inline-block flex-none self-start mt-2">
+								{/* share svg */}
+								<span className="inline ml-1" onClick={shareIdea}>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 512 512"
+										className="w-5 h-5  text-gray-500 fill-current inline mr-2"
+									>
+										<path
+											d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z"
+											// fill="#1A1A1A"
+										/>
+									</svg>
+								</span>
 								<span
 									className={`mt-1 mb-1 ml-1 text-xs font-medium inline uppercase text-${rColor}`}
 								>
@@ -180,25 +195,6 @@ const IdeaCard = ({ idea, history, pageTitle }) => {
 								{idea.title}
 							</h3>
 							<div className="inline-block flex-none self-start mt-2">
-								{/* <ShareBtn
-									url={"https://ideastorm.app/explore/" + idea.id}
-									text={"Check out this cool idea: " + idea.title}
-									className="ib"
-									displayText="Share"
-								/> */}
-								{/* share svg */}
-								{/* <span className="inline mr-1">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 512 512"
-										className="w-5 h-5  text-gray-600 fill-current inline mr-2"
-									>
-										<path
-											d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z"
-											// fill="#1A1A1A"
-										/>
-									</svg>
-								</span> */}
 								<span
 									className={`mt-1 mb-1 ml-1 text-xs font-medium inline uppercase text-${rColor}`}
 								>
