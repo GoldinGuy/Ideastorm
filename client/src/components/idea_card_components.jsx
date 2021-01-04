09e4b1a5bb50c3abecc6df557dec11a6f1c52a67
Seth <sethgoldin27@gmail.com>
2021-01-04 16:47:10 -0500

    test custom close icon mobile

--- client/src/components/idea_card_components.jsx
+++ client/src/components/idea_card_components.jsx
@@ -96,6 +96,16 @@ const IdeaCard = ({ idea, history, pageTitle }) => {
 	// 	setModal(true);
 	// }
 
+	const closeIcon = (
+		<svg fill="currentColor" viewBox="0 0 20 20" width={28} height={28}>
+			<path
+				fillRule="evenodd"
+				d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+				clipRule="evenodd"
+			></path>
+		</svg>
+	);
+
 	return (
 		<>
 			<div
@@ -197,6 +207,7 @@ const IdeaCard = ({ idea, history, pageTitle }) => {
 			{/* modal */}
 			<Modal
 				open={modalOpen}
+				closeIcon={closeIcon}
 				onClose={() => {
 					if (pageTitle === "Latest" || pageTitle === "Trending") {
 						history.push({
@@ -224,7 +235,7 @@ const IdeaCard = ({ idea, history, pageTitle }) => {
 							<div className="inline-block flex-none self-start mt-2">
 								{/* Share */}
 								<button
-									className="inline ml-1 mr-2 focus:border-0 focus:outline-none"
+									className="inline ml-1 mr-1 focus:border-0 focus:outline-none"
 									onClick={shareIdea}
 								>
 									<svg
